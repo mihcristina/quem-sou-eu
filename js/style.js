925a4effd84e03942a8a255da6ae6fe0cda608a3
Michelli <contato@michellicristina.com.br>
2021-09-11 17:00:33 -0300

    fix: js, delete bootstrap

--- js/style.js
+++ js/style.js
@@ -1,3 +1,5 @@
+// Título
+
 function typeWriter(elemento) {
     const textoArray = elemento.innerHTML.split('');
     elemento.innerHTML = '';
@@ -8,3 +10,24 @@ function typeWriter(elemento) {
 }
     const titulo = document.querySelector('#titulo_dois');
     typeWriter(titulo);
+
+// Scroll suave
+
+const menuItens = document.querySelectorAll('.menu a')
+
+menuItens.forEach(item => {
+    item.addEventListener('click', scrollClick)
+})
+
+function scrollClick(event) {
+    event.preventDefault()
+    const elemento = event.target;
+    const id = elemento.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+
+    window.scroll({
+        top: to - 80,
+        behavior: "smooth",
+
+    });
+}
